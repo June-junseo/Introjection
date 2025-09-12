b1a6fc2acf60bf955e4002486c632adeaccda055
June <heojunser@gmail.com>
2025-09-12 17:24:51 +0900

    dagger skill 구현 완

--- My project/Assets/Scripts/PoolManager.cs
+++ My project/Assets/Scripts/PoolManager.cs
@@ -3,25 +3,26 @@ using System.Collections.Generic;
 
 public class PoolManager : MonoBehaviour
 {
-    public GameObject[] prefabs;
-
+    public GameObject[] prefabs; 
     private List<GameObject>[] pools;
 
     private void Awake()
     {
-        for(int i = 0; i < pools.Length; i++)
+        pools = new List<GameObject>[prefabs.Length];
+
+        for (int i = 0; i < pools.Length; i++)
         {
             pools[i] = new List<GameObject>();
         }
-        // list √ ±‚»≠
     }
 
-    private GameObject Get(int index)
+    public GameObject Get(int index)
     {
         GameObject select = null;
-        foreach(var item in pools[index])
+
+        foreach (var item in pools[index])
         {
-            if(!item.activeSelf)
+            if (!item.activeSelf)
             {
                 select = item;
                 select.SetActive(true);
@@ -33,12 +34,8 @@ public class PoolManager : MonoBehaviour
         {
             select = Instantiate(prefabs[index], transform);
             pools[index].Add(select);
-
         }
 
         return select;
-
     }
-
-
 }
--- My project/Assets/Scripts/Reposition.cs
+++ My project/Assets/Scripts/Reposition.cs
@@ -19,55 +19,6 @@ public class Reposition : MonoBehaviour
                 player = p.GetComponent<Player>();
         }
     }
-
-
-    //private void OnTriggerExit2D(Collider2D collision)
-    //{
-    //    if (!collision.CompareTag("Area"))
-    //    {
-    //        return;
-    //    }
-
-    //    Vector3 playerPos = player.transform.position;
-    //    Vector3 myPos = transform.position;
-
-    //    float diffX = Mathf.Abs(playerPos.x - myPos.x);
-    //    float diffY = Mathf.Abs(playerPos.y - myPos.y);
-
-    //    Vector3 playerDir = player.vec;
-
-    //    float dirX = playerDir.x > 0 ? 1 : -1;
-    //    float dirY = playerDir.y > 0 ? 1 : -1;
-
-    //    switch (transform.tag)
-    //    {
-    //        case "Ground":
-
-    //            if (diffX > diffY)
-    //            {
-    //                transform.Translate(Vector3.right * dirX * 80);
-    //            }
-    //            else if (diffX < diffY)
-    //            {
-    //                transform.Translate(Vector3.up * dirY * 80);
-    //            }
-    //            //else
-    //            //{
-    //            //    transform.Translate(Vector3.right * dirX * 80);
-    //            //    transform.Translate(Vector3.up * dirY * 80);
-    //            //}
-    //            break;
-
-    //        case "Monster":
-    //            if (coll.enabled)
-    //            {
-    //                transform.Translate(playerDir * 30 +
-    //                    new Vector3(Random.Range(-3f, 3f),
-    //                                Random.Range(-3f, 3f), 0f));
-    //            }
-    //            break;
-    //    }
-    //}
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
@@ -80,12 +31,12 @@ public class Reposition : MonoBehaviour
         float diffY = Mathf.Abs(playerPos.y - myPos.y);
 
         Vector3 playerDir = player.vec;
-        if (playerDir == Vector3.zero) return; // 플레이어가 멈춰있을 때 이동 방지
+        if (playerDir == Vector3.zero) return; 
 
         float dirX = playerDir.x > 0 ? 1 : (playerDir.x < 0 ? -1 : 0);
         float dirY = playerDir.y > 0 ? 1 : (playerDir.y < 0 ? -1 : 0);
 
-        float tileSize = 40f; // 실제 타일 크기로 변경 필요
+        float tileSize = 48f; 
 
         switch (transform.tag)
         {
